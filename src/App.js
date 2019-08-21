266dbadc9104d362c177167159e671bf43689275
boramriganka <42547729+boramriganka@users.noreply.github.com>
2019-08-21 21:01:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     if (city && country) {
+      console.log(data);
       this.setState({
         temperature: data.main.temp,
         city: data.name,
