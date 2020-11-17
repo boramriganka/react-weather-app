a1c3fed7341bdc9196df1e65d1d2b5fe6f171fa1
boramriganka <42547729+boramriganka@users.noreply.github.com>
2020-11-17 13:58:56 +0530

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,13 +3,43 @@ import React from "react";
 const Weather = props => (
 	<div className="weather__info">
 	 {	
-	 	props.city && props.country && <p className="weather__key"> Location: 
+	 	props.city && props.country && <div className="weather__key"> Location: 
 	 		<span className="weather__value"> { props.city }, { props.country }</span>
-	 	</p> 
+	 	</div> 
 	 }
 	 { 	
-	 	props.temperature && <p className="weather__key"> Temperature: 
+	 	props.temperature && <div className="weather__key"> Temperature: 
 	 		<span className="weather__value"> { props.temperature }	</span>
+	 	</div> 
+   }
+   { 	
+	 		props.temperature  && <div className="weather__key"> minimum temperature: 
+	 		<span className="weather__value"> { props.min_temp }	</span>
+	 	</div> 
+   }
+   { 	
+	 		props.temperature && <div className="weather__key"> maximum temperature: 
+     <span className="weather__value"> { props.max_temp }	</span>
+   </div> 
+   }
+   { 	
+	 		props.city && props.country  && <div className="weather__key"> It feels like: 
+	 		<span className="weather__value"> { props.feels_like } here	</span>
+	 	</div> 
+   }
+   { 	
+	 		props.city && props.country && <div className="weather__key"> The pressure is : 
+	 		<span className="weather__value"> { props.pressure }	</span>
+	 	</div> 
+   }
+   { 	
+	 	props.city && props.country  && <div className="weather__key"> The wind is blowing at : 
+	 		<span className="weather__value"> { props.wind_speed } km/h	</span>
+	 	</div> 
+   }
+     { 	
+	 		props.city && props.country  && <p className="weather__key"> Direction of wind is : 
+	 		<span className="weather__value"> { props.wind_direction} degree	</span>
 	 	</p> 
 	 }
 	 { 	
